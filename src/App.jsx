a4aac682caa8fd2e6b93f08779dd9270f4d173b9
SSO <sso@d.co>
2025-04-24 16:07:37 +0200

    added NewProject component and stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import NewProject from './components/NewProject';
+import ProjectsSidebar from './components/ProjectsSidebar';
+
 function App() {
-  return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+	return (
+		<main className='h-screen my-8 flex gap-8'>
+			<ProjectsSidebar />
+      <NewProject />
+		</main>
+	);
 }
 
 export default App;
