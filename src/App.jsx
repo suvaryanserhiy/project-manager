14e6b6c07e5d75d9f099a24dedd8b4c3e1cbf69c
SSO <sso@d.co>
2025-04-24 16:28:19 +0200

    new component NoProjectSelected, dynamically switch between new component and NewProject componet depending if (create new project) btn was clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import NewProject from './components/NewProject';
+import NoProjectSelected from './components/NoProjectSelected';
 import ProjectsSidebar from './components/ProjectsSidebar';
 
 function App() {
+	const [projectsState, setProjectsState] = useState({
+		selectedProjectId: undefined, //means that we are doing nothing
+		projects: [],
+	});
+	function handleStateAddProject() {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: null, // means that we are adding new project
+			};
+		});
+	}
+
+	let content;
+	if (projectsState.selectedProjectId === null) {
+		content = <NewProject />;
+	} else if (projectsState.selectedProjectId === undefined) {
+		content = <NoProjectSelected onStartAddProject={handleStateAddProject} />;
+	}
 	return (
 		<main className='h-screen my-8 flex gap-8'>
-			<ProjectsSidebar />
-      <NewProject />
+			<ProjectsSidebar onStartAddProject={handleStateAddProject} />
+			{content}
 		</main>
 	);
 }
