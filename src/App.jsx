81832431f82a12eff2d6129297104c1930f06f1e
SSO <sso@d.co>
2025-04-25 23:26:27 +0200

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,33 @@ function App() {
 	const [projectsState, setProjectsState] = useState({
 		selectedProjectId: undefined, //means that we are doing nothing
 		projects: [],
+		tasks: [],
 	});
+
+	function handleAddTask(text) {
+		setProjectsState((prevState) => {
+			const taskId = Math.random();
+			const newTask = {
+				text: text,
+				projectId: prevState.selectedProjectId,
+				id: taskId,
+			};
+			return {
+				...prevState,
+				tasks: [newTask, ...prevState.tasks],
+			};
+		});
+	}
+
+	function handleDeleteTask(id) {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				tasks: prevState.tasks.filter((task) => task.id !== id),
+			};
+		});
+	}
+
 	function handleStateAddProject() {
 		setProjectsState((prevState) => {
 			return {
@@ -48,10 +74,30 @@ function App() {
 			};
 		});
 	}
+
+	function handleDeleteProject() {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: undefined,
+				projects: prevState.projects.filter(
+					(project) => project.id !== prevState.selectedProjectId
+				),
+			};
+		});
+	}
 	const selectedProject = projectsState.projects.find(
 		(project) => project.id === projectsState.selectedProjectId
 	);
-	let content = <SelectedProject project={selectedProject} />;
+	let content = (
+		<SelectedProject
+			project={selectedProject}
+			onDelete={handleDeleteProject}
+			onAddTask={handleAddTask}
+			onDeleteTask={handleDeleteTask}
+			tasks={projectsState.tasks}
+		/>
+	);
 	if (projectsState.selectedProjectId === null) {
 		content = (
 			<NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
@@ -65,6 +111,7 @@ function App() {
 				onStartAddProject={handleStateAddProject}
 				projects={projectsState.projects}
 				onSelectProject={handleSelectProject}
+				selectedProjectId={projectsState.selectedProjectId}
 			/>
 			{content}
 		</main>
